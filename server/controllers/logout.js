58a11a196a8f49cfa07398006217d4674dab2b30
leongshengmin <leongshengmin@gmail.com>
2019-02-27 11:31:41 +0800

    bugfix logout

--- server/controllers/logout.js
+++ server/controllers/logout.js
@@ -3,7 +3,7 @@
 let index = (req, res) => {
     if (req.isAuthenticated()) {
         req.logout();
-        return req.redirect('/');
+        return res.redirect('/');
     }
     console.log('user not currently logged in');
 };
