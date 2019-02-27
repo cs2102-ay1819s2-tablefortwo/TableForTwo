02900e66b230e7e301034daff7d25e7a49e42aca
Koh Jun Wei <kjunwei1996@gmail.com>
2019-02-27 15:18:58 +0800

    Change login route

--- server/routes/home.js
+++ server/routes/home.js
@@ -5,6 +5,6 @@ const homeController = require('../controllers/home');
 let router = express.Router();
 
 router.get('/', homeController.index);
-router.post('/', homeController.handleLoginValidation);
+router.post('/login', homeController.handleLoginValidation);
 
 module.exports = router;
