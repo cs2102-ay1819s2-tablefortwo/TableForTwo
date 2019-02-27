e5ce94cab4330cd91dfa2548acedae4df49c5d04
Koh Jun Wei <kjunwei1996@gmail.com>
2019-02-27 15:15:46 +0800

    Refactor views

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,8 +1,8 @@
 function check(event) {
     // Get Values
-    var name = document.getElementById('name').value;
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
+    var name = document.getElementById('loginName').value;
+    var username = document.getElementById('loginUsername').value;
+    var password = document.getElementById('loginPassword').value;
 
     // Simple Check
     if (name.length == 0 || username.length == 0 || password.length == 0) {
