f6c2926a108d9cfd4daca08a6b3527bd443c66c2
leongshengmin <leongshengmin@gmail.com>
2019-02-28 09:07:25 +0800

    add event listener for branch

--- public/javascripts/restaurantView.js
+++ public/javascripts/restaurantView.js
@@ -1,5 +1,5 @@
 const branchController = require('../../server/controllers/branch');
 
-let passDataToBranchController = (bid) => {
-    console.log('passing data from restaurant view to controller');
+function check() {
+    alert();
 }
