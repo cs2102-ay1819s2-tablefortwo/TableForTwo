35def1ef13b75b5f2844a1f2770158c5b85a358a
Koh Jun Wei <kjunwei1996@gmail.com>
2019-03-26 14:25:52 +0800

    Remove Owns query

--- sqlQueries/restaurants.js
+++ sqlQueries/restaurants.js
@@ -1,7 +1,6 @@
 let sqlQueries = {
 	allRestaurants: 'SELECT * FROM Restaurants',
-	allRestaurantsAndBranches: 'select * from branches left join (select * from restaurants inner join owns on restaurants.id = owns.restaurant_id)t_CTE on branches.id = t_CTE.branch_id;',
-    getAssocBranches: 'select * from branches where id in (select branch_id from owns where restaurant_id = $1);',
+	allRestaurantsAndBranches: 'SELECT * FROM Restaurants r INNER JOIN Branches b on r.id = b.restaurant_id;',
     getBranchMenuItems: 'select * from branches right join sells on branches.id = sells.bid right join menuitems on menuitems.id = sells.mid where branches.id = $1;'
 };
 
