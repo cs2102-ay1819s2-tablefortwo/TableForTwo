9cbc13441d0e1dad76ba9fa6c33563279481da0f
yuheong <yuhe_ong@hotmail.com>
2019-04-11 10:51:19 +0800

    Fix merge conflicts

--- server/routes/manage.js
+++ server/routes/manage.js
@@ -6,9 +6,6 @@ let router = express.Router();
 
 router.get('/', manageController.getOverview);
 router.post('/deleteslot', manageController.deleteSlot);
-<<<<<<< HEAD
 router.post('/addSlot', manageController.addSlot);
-=======
->>>>>>> 695cd6a42a63c4d9d5db74125ac699aa595358ee
 
 module.exports = router;
